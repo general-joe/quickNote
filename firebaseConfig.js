e3bd0aa9320913688f4738139fedfad26cfbae26
D-General <kwabijoe274@gmail.com>
2025-05-22 06:48:51 +0000

    Bulit landing page and hero-section and some bugs were fixed

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8-rWeM7i93kR2n3a9HJ1uthIsGh4e1E0",
+  authDomain: "quick-notes-f08ff.firebaseapp.com",
+  projectId: "quick-notes-f08ff",
+  storageBucket: "quick-notes-f08ff.firebasestorage.app",
+  messagingSenderId: "599373721071",
+  appId: "1:599373721071:web:241673d4566a6743da1a1a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Landing from "./routes/Landing";
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Signup from "./routes/Signup";
+import Login from "./routes/Login";
 function App() {
   return (
     <>
-      <h1 className="bg-purple-100">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
     </>
   );
 }
